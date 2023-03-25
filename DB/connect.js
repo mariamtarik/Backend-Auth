51a94f6f8e7958767c25a70b1038a8eb3f64580a
mariam <mariamtarik799@gmail.com>
2023-03-25 04:48:56 +0300

    change error in connect.js file

--- DB/connect.js
+++ DB/connect.js
@@ -1,10 +1,12 @@
 const mongoose=require("mongoose");
 
 const connectDB=()=>{
-    
-return mongoose.connect(process.env.DB_URL).then((res)=>{
-console.log("DB connected...")
-    }).catch((err)=>{console.log("failed connected",...err)})
-}
+
+    return mongoose.connect(process.env.DB_URL).then((res)=>{
+        console.log("DB connected...")
+            }).catch((err)=>{console.log("failed connected",err)})
+        } 
+
+
 
 module.exports=connectDB;
